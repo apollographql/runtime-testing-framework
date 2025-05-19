5556d6efd607d2d8bf0dcd3bff96b577a8e68d76
dandersonhall <67905850+dandersonhall@users.noreply.github.com>
2025-05-19 15:03:20 +0100

    Update apply_values test to use a macro and simple_test_case for setting the test parameters

--- crates/rtf-config/src/formats/mod.rs
+++ crates/rtf-config/src/formats/mod.rs
@@ -33,6 +33,7 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// A helper function for replacing templated value strings in config with their actual values.
 /// This takes a map of value keys, constructs the expected template format of {{ value.<key> }},
 /// looks for this in the provided config string and replaces it with the actual value
+#[allow(dead_code)]
 pub(crate) fn apply_values(
     config: impl Into<String>,
     values: &HashMap<String, serde_json::Value>,
@@ -53,80 +54,72 @@ pub(crate) fn apply_values(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use serde_json::{Value, json};
+    use serde_json::Value;
+    use simple_test_case::test_case;
 
-    // TO DO: Make this into a macro so I can pass in values other than strings
-    fn make_map(values: &[(&str, &str)]) -> HashMap<String, Value> {
-        values
-            .into_iter()
-            .map(|(k, v)| (k.to_string(), Value::String(v.to_string())))
-            .collect()
+    // Helper macro to create a HashMap<String, serde_json::Value> where the Value can be any valid json object.
+    // Intended to be used for easily creating test values for testing templating
+    macro_rules! values_map {
+        () => {
+            ::std::collections::HashMap::<String, ::serde_json::Value>::new()
+        };
+
+        ($($k:expr => $v:expr),+) => {{
+            let mut m = ::std::collections::HashMap::new();
+
+            $(
+                m.insert($k.to_string(), ::serde_json::json!($v));
+            )+
+
+            m
+        }};
     }
 
+    #[test_case(
+        values_map!(),
+        "parameters:\n  some_param: \"foo\"",
+        "parameters:\n  some_param: \"foo\"";
+        "empty_value"
+    )]
+    #[test_case(
+        values_map!("foo" => "bar"),
+        "parameters:\n  some_param: \"{{ foo }}\"",
+        "parameters:\n  some_param: \"bar\"";
+        "single_string_value"
+    )]
+    #[test_case(
+        values_map!("foo" => 42),
+        "parameters:\n  some_param: \"{{ foo }}\"",
+        "parameters:\n  some_param: 42";
+        "single_integer_value"
+    )]
+    #[test_case(
+        values_map!("foo" => 42.42),
+        "parameters:\n  some_param: \"{{ foo }}\"",
+        "parameters:\n  some_param: 42.42";
+        "single_float_value"
+    )]
+    #[test_case(
+        values_map!("foo" => true),
+        "parameters:\n  some_param: \"{{ foo }}\"",
+        "parameters:\n  some_param: true";
+        "single_bool_value"
+    )]
+    #[test_case(
+        values_map!("foo" => "bar", "baz" => "qux"),
+        "parameters:\n  some_param: \"{{ foo }}\"\n  another_param: \"{{ baz }}\"",
+        "parameters:\n  some_param: \"bar\"\n  another_param: \"qux\"";
+        "multiple_string_values"
+    )]
+    #[test_case(
+        values_map!("foo" => "bar"),
+        "parameters:\n  some_param: \"{{ foo }}\"\n  another_param: \"{{ foo }}\"",
+        "parameters:\n  some_param: \"bar\"\n  another_param: \"bar\"";
+        "same_string_value_multiple_times"
+    )]
     #[test]
-    fn apply_single_string_to_template() {
-        let values = make_map(&[("foo", "bar")]);
-        let config = "parameters:\n  some_param: \"{{ foo }}\"".to_string();
-
-        let config = apply_values(&config, &values);
-        assert_eq!(config, "parameters:\n  some_param: \"bar\"")
-    }
-
-    #[test]
-    fn apply_single_integer_to_template() {
-        let mut values = HashMap::new();
-        values.insert("foo".to_string(), json!(true));
-        let config = "parameters:\n  some_param: \"{{ foo }}\"".to_string();
-
-        let config = apply_values(&config, &values);
-        assert_eq!(config, "parameters:\n  some_param: true")
-    }
-
-    #[test]
-    fn apply_single_float_to_template() {
-        let mut values = HashMap::new();
-        values.insert("foo".to_string(), json!(42.42));
-        let config = "parameters:\n  some_param: \"{{ foo }}\"".to_string();
-
-        let config = apply_values(&config, &values);
-        assert_eq!(config, "parameters:\n  some_param: 42.42")
-    }
-
-    #[test]
-    fn apply_single_bool_to_template() {
-        let mut values = HashMap::new();
-        values.insert("foo".to_string(), json!(42.42));
-        let config = "parameters:\n  some_param: \"{{ foo }}\"".to_string();
-
-        let config = apply_values(&config, &values);
-        assert_eq!(config, "parameters:\n  some_param: 42.42")
-    }
-
-    #[test]
-    fn apply_multiple_values_to_template() {
-        let values = make_map(&[("foo", "bar"), ("hello", "goodbye")]);
-
-        let config = "parameters:\n  some_param: \"{{ foo }}\"\n  another_param: \"{{ hello }}\""
-            .to_string();
-
-        let config = apply_values(&config, &values);
-        assert_eq!(
-            config,
-            "parameters:\n  some_param: \"bar\"\n  another_param: \"goodbye\""
-        )
-    }
-
-    #[test]
-    fn apply_same_value_to_template_multiple_times() {
-        let values = make_map(&[("foo", "bar")]);
-        let config =
-            "parameters:\n  some_param: \"{{ foo }}\"\n  another_param: \"{{ foo }}\"".to_string();
-
-        let config = apply_values(&config, &values);
-
-        assert_eq!(
-            config,
-            "parameters:\n  some_param: \"bar\"\n  another_param: \"bar\""
-        )
+    fn apply_template_values_works(values: HashMap<String, Value>, config: &str, expected: &str) {
+        let config = apply_values(config, &values);
+        assert_eq!(config, expected)
     }
 }
