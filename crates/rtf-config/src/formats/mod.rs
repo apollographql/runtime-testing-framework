68ef03a027e795e922d8ef59775fcbb0d38b23f4
Innes Anderson-Morrison <innes.andersonmorrison@apollographql.com>
2025-05-28 11:35:29 +0100

    RR-79 remove unused test plan code pending rewrite

--- crates/rtf-config/src/formats/mod.rs
+++ crates/rtf-config/src/formats/mod.rs
@@ -4,11 +4,9 @@ use std::{collections::HashMap, io};
 
 mod environment;
 mod scenario;
-mod test_plan;
 
 pub use environment::EnvironmentConfig;
 pub use scenario::ScenarioConfig;
-pub use test_plan::{BaseTestPlanConfig, RawBaseTestPlanConfig};
 
 /// Errors that can be encountered resolving config files
 #[derive(Debug, thiserror::Error)]
