098ccb464773d34738b465c2ff976f31826c07fb
dandersonhall <67905850+dandersonhall@users.noreply.github.com>
2025-06-12 09:25:10 +0100

    Split router-scale vm create and delete scripts
    It is simpler to run the create and delete scripts for router-scale as separate, self-contained scripts. Dependency checks happen in the create script.
    
    The CommandSpec is used to run the commands instead of a String so the VM name is specified in the values.

--- example-test-plans/router-scale/wrapper/scripts/create-router-scale-vm.sh
+++ example-test-plans/router-scale/wrapper/scripts/create-router-scale-vm.sh
@@ -4,11 +4,6 @@ shopt -s expand_aliases
 
 BASE_DIR=$(dirname $0)
 
-###
-# Check we have the required dependencies
-####
-source "${BASE_DIR}/helpers/local-dependencies.sh" && check_dependencies || exit 2
-
 alias GCLOUD_SA="gcloud --impersonate-service-account=sa-router-performance@router-performance.iam.gserviceaccount.com"
 
 # Used to filter out annoying impersonation warning message
@@ -17,6 +12,50 @@ FILTER_OUT="WARNING: This command is using service account impersonation."
 # Used to log output to text file
 OUTDIR="${OUTDIR:-$(pwd)}"
 LOG_FILE="$OUTDIR/vm-log.txt"
+
+###
+# Dependencies required to run local router-scale scripts:
+#
+#  It's important to keep this list up to date
+###
+dependencies=("gcloud" "rsync")
+
+###
+# report
+#
+# Report any missing commands.
+#
+# $*: Missing commands
+###
+function report {
+    printf "You need the following components to use this command:\n"
+    printf "\t %s\n" "${dependencies[@]}"
+    printf "You are missing %s\n" "$*"
+    printf "Some of these components can be installed from homebrew\n"
+    return 1
+}
+
+###
+# Check we have the required dependencies
+####
+function check_dependencies {
+    # Before checking dependencies make sure we are running bash 5 at least
+    if ! bash --version | grep -q 'version 5\.'; then
+        printf "Bash v5 is required to run these scripts.\n"
+        printf "Note: If this is the future and we now have Bash 6, update this check\n"
+        printf "Note: You can easily install Bash 5 from homebrew\n"
+        return 1
+    fi
+    missing=()
+    for dependency in "${dependencies[@]}"; do
+        if ! which "${dependency}" > /dev/null 2>&1; then
+            missing+=("${dependency}")
+        fi
+    done
+    if (( ${#missing[@]} != 0 )); then
+        report "${missing[@]}"
+    fi
+}
 
 ###
 # Create an instance.
@@ -54,55 +93,12 @@ function create_vm {
     fi
 }
 
-###
-# List instance
-###
-function list_vm {
-    GCLOUD_SA compute instances list \
-        --filter="zone ~ us AND labels.perf-test:*" \
-        --project router-performance \
-        2> >(grep -v "${FILTER_OUT}")
-}
+if [ -z "$1" ]; then
+    echo "Usage: $0 <vm-name>"
+    exit 1
+fi
+check_dependencies || exit 2
+create_vm "$1" >> $LOG_FILE
 
-###
-# Delete an instance
-###
-function delete_vm {
-    echo "Deleting test system: $1..."
-    GCLOUD_SA compute instances delete "$1" \
-        --quiet \
-        --project=router-performance \
-        --zone=us-central1-b \
-        2> >(grep -v "${FILTER_OUT}")
-    echo "Test system: $1 deleted."
-}
-
-case "$1" in
-    create)
-        if [ -z "$2" ]; then
-            echo "Usage: $0 create <vm-name>"
-            exit 1
-        fi
-        create_vm "$2" >> $LOG_FILE
-
-        # Echo empty output so RTF environment setup succeeds
-        echo {}
-        ;;
-    delete)
-        if [ -z "$2" ]; then
-            echo "Usage: $0 delete <vm-name>"
-            exit 1
-        fi
-        delete_vm "$2" >> $LOG_FILE
-
-        # Echo empty output so RTF environment teardown succeeds
-        echo {}
-        ;;
-    list)
-        list_vm
-        ;;
-    *)
-        echo "Usage: $0 {create <vm-name>|delete <vm-name>|list}"
-        exit 1
-        ;;
-esac
+# Echo empty output so RTF environment setup succeeds
+echo {}
