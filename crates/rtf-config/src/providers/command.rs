565c6cb4ee52076033f6e063acf51ae07c0a3913
dandersonhall <67905850+dandersonhall@users.noreply.github.com>
2025-06-12 11:43:01 +0100

    Add CommandProvider
    Add a CommandProvider enum that wraps a subset of the FileProviders. This will allow the commands to be specified using files. Not all file providers will result in executable command files which is why we use a subset.

--- crates/rtf-config/src/providers/command.rs
+++ crates/rtf-config/src/providers/command.rs
@@ -1,8 +1,10 @@
 use crate::{
     context::ResolutionContext,
     providers::{
-        self,
-        file::{AsUtf8FileContent, NamedFileProvider, Source},
+        self, Result,
+        file::{
+            AsUtf8FileContent, InlineFile, NamedFileProvider, RelativeFile, RequiredFile, Source,
+        },
     },
     templating::{self, Field, Scalar, Template},
     validation::{self, Validate, duplicate_keys},
@@ -195,6 +197,73 @@ impl RawCommand {
         match self {
             RawCommand::Raw(s) => s.as_str(),
         }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case", tag = "kind")]
+pub enum CommandProvider {
+    Inline(InlineFile),
+    RelativePath(RelativeFile),
+    Required(RequiredFile),
+}
+
+// Helper for generating boilerplate method impls where we just need to defer to the inner type
+// that a CommandProvider is wrapping.
+macro_rules! delegate_to_inner {
+    ($self:ident, $method:ident $(, $arg:expr)*) => {
+        match $self {
+            CommandProvider::Inline(fp) => fp.$method($($arg),*),
+            CommandProvider::RelativePath(fp) => fp.$method($($arg),*),
+            CommandProvider::Required(fp) => fp.$method($($arg),*),
+        }
+    };
+
+    (@async $self:ident, $method:ident, $($arg:expr),*) => {
+        match $self {
+            CommandProvider::Inline(fp) => fp.$method($($arg),*).await,
+            CommandProvider::RelativePath(fp) => fp.$method($($arg),*).await,
+            CommandProvider::Required(fp) => fp.$method($($arg),*).await,
+        }
+    };
+}
+
+impl AsUtf8FileContent for CommandProvider {
+    async fn try_get_file_content(
+        &self,
+        src: &Source,
+        ctx: &impl ResolutionContext,
+    ) -> Result<String> {
+        delegate_to_inner!(@async self, try_get_file_content, src, ctx)
+    }
+}
+
+impl Template for CommandProvider {
+    fn has_pending_fields(&self) -> bool {
+        delegate_to_inner!(self, has_pending_fields)
+    }
+
+    fn required_values(&self) -> Vec<String> {
+        delegate_to_inner!(self, required_values)
+    }
+
+    fn try_resolve(
+        &mut self,
+        path: &mut Vec<String>,
+        values: &HashMap<String, Scalar>,
+    ) -> templating::Result<()> {
+        delegate_to_inner!(self, try_resolve, path, values)
+    }
+}
+
+impl Validate for CommandProvider {
+    fn try_validate(
+        &self,
+        path: &mut Vec<String>,
+        src: &Source,
+        ctx: &impl ResolutionContext,
+    ) -> validation::Result<()> {
+        delegate_to_inner!(self, try_validate, path, src, ctx)
     }
 }
 
