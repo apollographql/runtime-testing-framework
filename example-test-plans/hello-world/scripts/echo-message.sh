a6eee4fb2c5e80785f0c225fadb4daadc0d3713f
Innes Anderson-Morrison <innes.andersonmorrison@apollographql.com>
2025-06-27 15:27:15 +0100

    RR-139 Append to the combined output file rather than overwriting

--- example-test-plans/hello-world/scripts/echo-message.sh
+++ example-test-plans/hello-world/scripts/echo-message.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-echo "${STAGE} :: ${MESSAGE}${SUBJECT}" | tee "$OUTDIR/combined-output.txt"
+echo "${STAGE} :: ${MESSAGE}${SUBJECT}" | tee -a "$OUTDIR/combined-output.txt"
 
 if [ "${STAGE}" = "env-setup" ]; then
   echo '{}' > "$RTF_OUTPUT"
--- example-test-plans/hello-world/scripts/teardown.sh
+++ example-test-plans/hello-world/scripts/teardown.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-echo "---" | tee "$OUTDIR/combined-output.txt"
+echo "---" | tee -a "$OUTDIR/combined-output.txt"
