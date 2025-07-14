2f3c6f072c05719d92f42e89bcfe17c11dff39bb
carodewig <16093297+carodewig@users.noreply.github.com>
2025-07-14 12:46:27 -0400

    Duplicate existing router-scale plan

---
+++ example-test-plans/router-scale-response-caching/router-perf/scripts/router-perf-teardown.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+
+OUTDIR="${OUTDIR:-$(pwd)}"
+RESULTS_DIR="$OUTDIR/results"
+
+echo "extracting data from redis"
+
+# Extract information about redis utilization (we do this before we shut down redis)
+echo "INFO" | redis-cli > "${RESULTS_DIR}/redis.info"
+# Just grab 100 keys from redis. Might not be helpful, but downloading the full dataset is definitely not useful
+echo 'SCAN 0 MATCH "*" COUNT 100' | redis-cli > "${RESULTS_DIR}/redis.keys"
+# Extract information about redis cluster utilization (we do this before we shut down redis cluster)
+echo "INFO" | redis-cli -c --pass router -p 6385 > "${RESULTS_DIR}/redis-cluster.info"
+# Just grab 100 keys from redis. Might not be helpful, but downloading the full dataset is definitely not useful
+echo 'SCAN 0 MATCH "*" COUNT 100' | redis-cli -c --pass router -p 6385 > "${RESULTS_DIR}/redis-cluster.keys"
+
+# We may have prometheus metrics to harvest, let's try the default location
+curl http://127.0.0.1:9090/metrics > "${RESULTS_DIR}/prometheus.metrics"
+
+echo "stopping the router"
+# Sometimes a router won't shutdown: we give it 5 seconds and then we're more forceful
+pkill -x router
+timeout 5s tail --pid="${ROUTER_PID}" -f /dev/null || kill -3 "${ROUTER_PID}"
+
+echo "stopping additional services"
+pkill -3 -x top
+pkill -3 -x strace
+pkill -3 -x router-side
+pkill -x subgraph
+pkill -x snapshot
+docker kill otel
+docker kill redis
+docker compose -f scale/scripts/docker-compose-redis.yml down > /dev/null 2>&1
+
+echo "removing cgroups"
+if [ "$ROUTER_CGROUP" = "true" ]; then
+  sudo cgdelete cpu,memory:/router/leaf
+  sudo cgdelete cpu,memory:/router
+fi
+
+sudo cgdelete cpu,memory:/subgraph/leaf
+sudo cgdelete cpu,memory:/subgraph
+
+echo "processing top output"
+jc -q --top-s < "${RESULTS_DIR}/top.user"  > "${RESULTS_DIR}/top.user.json"
+jc -q --top-s < "${RESULTS_DIR}/top.router"  > "${RESULTS_DIR}/top.router.json"
+jc -q --top-s < "${RESULTS_DIR}/top.redis"  > "${RESULTS_DIR}/top.redis.json"
+jc -q --top-s < "${RESULTS_DIR}/top.router-side"  > "${RESULTS_DIR}/top.router-side.json"
+
+echo "removing temp data"
+# Cleanup or we will run out of disk space very quickly (-f so we don't get a warning if the file isn't present)
+rm -f perf.data
+rm -f /tmp/router.*
---
+++ example-test-plans/router-scale-response-caching/wrapper/scripts/run-test-plan.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script will be run on the VM. It installs rust and then runs rtf
+# We need to install rust since the router-scale VM only has rust installed for the router user
+# This was the easiest way to get started with running rtf on the router-scale VM.
+# We may run RTF under a different user in a future ticket.
+
+if ! which cargo >/dev/null 2>&1; then
+    echo "cargo not found, installing rust..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    . "$HOME/.cargo/env"
+else
+    echo "cargo is already installed."
+fi
+
+export APOLLO_SUDO="true"
+export APOLLO_KEY="$1"
+
+echo "Running test plan..."
+cargo build --manifest-path ./rtf/Cargo.toml
+cargo run --manifest-path ./rtf/Cargo.toml -- run test-data/test-plan.yaml --outdir output --values "values.json" 2>&1
+
+# Create an output/results dir in case the rtf run fails
+mkdir -p output/results
