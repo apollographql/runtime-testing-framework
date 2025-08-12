b7806fe7a7d3f69880bdf0cfa6a685823a39d178
Innes Anderson-Morrison <innes.andersonmorrison@apollographql.com>
2025-08-12 11:34:59 +0100

    Move yaml merging logic to lib.rs as pub(crate)

--- crates/rtf-config/src/formats/test_plan.rs
+++ crates/rtf-config/src/formats/test_plan.rs
@@ -3,6 +3,7 @@ use crate::{
     checks::{self, Check},
     context::ResolutionContext,
     formats::{EnvironmentConfig, Error, Result, ScenarioConfig},
+    merge_yaml,
     providers::{
         self,
         command::CommandSection,
@@ -504,41 +505,12 @@ where
                 let file_content = src.try_get_file_content(ctx).await?;
                 let mut base: serde_yaml::Value = serde_yaml::from_str(&file_content)?;
                 if overrides != serde_yaml::Value::Null {
-                    merge(overrides, &mut base);
+                    merge_yaml(overrides, &mut base);
                 }
 
                 Ok((serde_yaml::from_value(base)?, Some(src)))
             }
         }
-    }
-}
-
-/// A function for merging yaml overrides with the base config. It is expected
-/// behaviour that lists will be a combination of the base and override lists
-/// for the same key. This function performs no deduplication.
-fn merge(overrides: serde_yaml::Value, base: &mut serde_yaml::Value) {
-    use serde_yaml::Value;
-
-    match (overrides, base) {
-        // If both values are mappings we add all keys from src into dst.
-        (Value::Mapping(override_map), Value::Mapping(base_map)) => {
-            for (key, override_val) in override_map.into_iter() {
-                // If a key is present in both maps then we recursively merge the values,
-                // otherwise we just insert the src key into dst directly.
-                match base_map.get_mut(&key) {
-                    Some(base_val) => merge(override_val, base_val),
-                    None => _ = base_map.insert(key, override_val),
-                };
-            }
-        }
-
-        // If both values are sequences we append overrides to base
-        (Value::Sequence(override_seq), Value::Sequence(base_seq)) => {
-            base_seq.extend_from_slice(&override_seq)
-        }
-
-        // Otherwise we replace base with overrides
-        (overrides, base) => *base = overrides,
     }
 }
 
--- crates/rtf-config/src/lib.rs
+++ crates/rtf-config/src/lib.rs
@@ -28,3 +28,32 @@ pub struct ValueDefinition {
     #[serde(default)]
     pub default: Option<templating::Scalar>,
 }
+
+/// A function for merging yaml overrides with the base config. It is expected
+/// behaviour that lists will be a combination of the base and override lists
+/// for the same key. This function performs no deduplication.
+pub(crate) fn merge_yaml(overrides: serde_yaml::Value, base: &mut serde_yaml::Value) {
+    use serde_yaml::Value;
+
+    match (overrides, base) {
+        // If both values are mappings we add all keys from src into dst.
+        (Value::Mapping(override_map), Value::Mapping(base_map)) => {
+            for (key, override_val) in override_map.into_iter() {
+                // If a key is present in both maps then we recursively merge the values,
+                // otherwise we just insert the src key into dst directly.
+                match base_map.get_mut(&key) {
+                    Some(base_val) => merge_yaml(override_val, base_val),
+                    None => _ = base_map.insert(key, override_val),
+                };
+            }
+        }
+
+        // If both values are sequences we append overrides to base
+        (Value::Sequence(override_seq), Value::Sequence(base_seq)) => {
+            base_seq.extend_from_slice(&override_seq)
+        }
+
+        // Otherwise we replace base with overrides
+        (overrides, base) => *base = overrides,
+    }
+}
