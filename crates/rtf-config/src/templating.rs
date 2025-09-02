0e6252f5c6dd15b45e1ecbb5f5e1647e1819247c
dandersonhall <67905850+dandersonhall@users.noreply.github.com>
2025-09-02 12:15:59 +0100

    Add additional error messaging for serde untagged enums in templating

--- crates/rtf-config/src/templating.rs
+++ crates/rtf-config/src/templating.rs
@@ -425,7 +425,7 @@ impl fmt::Display for Number {
 ///
 /// A scalar that is valid to be used as a template value for a [Field].
 #[derive(Debug, Clone, PartialEq, Deserialize, Serialize, JsonSchema)]
-#[serde(untagged)]
+#[serde(untagged, expecting = "expecting a valid Number, Boolean or String")]
 pub enum Scalar {
     /// Represents a number, whether integer or floating point.
     Number(Number),
@@ -576,7 +576,10 @@ mod tests {
     }
 
     #[derive(Debug, PartialEq, Deserialize)]
-    #[serde(untagged)]
+    #[serde(
+        untagged,
+        expecting = "expecting a Field that can resolve into a usize, isize, f64, bool or String"
+    )]
     enum Target {
         U { usize: Field<usize> },
         I { isize: Field<isize> },
