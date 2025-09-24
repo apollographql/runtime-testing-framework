ef0009e5709f7b79bd0bf6fad33d88ce872f8bf8
Innes Anderson-Morrison <innes.andersonmorrison@apollographql.com>
2025-09-24 12:37:36 +0100

    Add a derive macro for the Template trait (#154)

---
+++ crates/rtf-derive/examples/expand-me.rs
@@ -0,0 +1,37 @@
+use rtf_config::templating::{Field, Template};
+use rtf_derive::Template;
+use std::collections::HashMap;
+
+#[derive(Debug, Template)]
+pub struct MyStruct {
+    pub foo: Field<String>,
+    pub bar: Bar,
+    #[template(skip)]
+    pub baz: String,
+}
+
+#[derive(Debug, Template)]
+pub struct Bar {
+    pub inner: Field<u32>,
+}
+
+fn main() {
+    let mut s = MyStruct {
+        foo: Field::Pending("FOO".to_string()),
+        bar: Bar {
+            inner: Field::Resolved(42),
+        },
+        baz: "BAZ".to_string(),
+    };
+
+    println!("{:?}", s.has_pending_fields());
+    println!("{:?}", s.required_values());
+    println!("{:?}", s);
+
+    let mut vals = HashMap::new();
+    vals.insert("FOO".to_string(), "a value for foo".into());
+
+    s.try_template(&mut Vec::new(), &vals).unwrap();
+    println!("{:?}", s.has_pending_fields());
+    println!("{:?}", s);
+}
