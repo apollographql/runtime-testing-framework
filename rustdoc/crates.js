19942a0c77ee3cf77c1ac50dd26aee22d8341b49
sminez <sminez@users.noreply.github.com>
2025-09-24 11:40:37 +0000

    deploy: ef0009e5709f7b79bd0bf6fad33d88ce872f8bf8

--- rustdoc/crates.js
+++ rustdoc/crates.js
@@ -1,2 +1,2 @@
-window.ALL_CRATES = ["rtf_cli","rtf_config","rtf_core"];
-//{"start":21,"fragment_lengths":[9,13,11]}
+window.ALL_CRATES = ["rtf_cli","rtf_config","rtf_core","rtf_derive"];
+//{"start":21,"fragment_lengths":[9,13,11,13]}
---
+++ rustdoc/search.desc/rtf_derive/rtf_derive-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("rtf_derive", 0, "")
