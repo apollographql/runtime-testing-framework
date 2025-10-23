25eaba8a7044a99777caf6bdfc06c24cc2053cc1
Dan Anderson Hall <67905850+dandersonhall@users.noreply.github.com>
2025-10-23 16:20:20 +0100

    Add an expand-matrix command (#191)

--- crates/rtf-cli/tests/cli_tests.rs
+++ crates/rtf-cli/tests/cli_tests.rs
@@ -3,6 +3,7 @@ pub mod common;
 use assert_cmd::Command;
 use common::{is_valid_test_plan, prepare_rtf_run};
 use predicates::str::contains;
+use serde_json::json;
 
 #[test]
 fn is_executable() {
@@ -12,6 +13,62 @@ fn is_executable() {
 
     // Check the output contains usage instructions for rtf
     res.stderr(contains("Usage: rtf"));
+}
+
+#[test]
+fn expand_matrix_command_is_executable() {
+    let mut cmd = Command::cargo_bin("rtf").unwrap();
+    let res = cmd.arg("expand-matrix").assert();
+
+    res.stderr(contains("Usage: rtf expand-matrix"));
+}
+
+#[test]
+fn expand_matrix_command_invalid_test_plan_path_errors() {
+    let mut cmd = Command::cargo_bin("rtf").unwrap();
+
+    let res = cmd.arg("expand-matrix").arg("/not/a/file.txt").assert();
+
+    res.stderr(contains("No such file or directory (os error 2)"));
+}
+
+#[test]
+fn expand_matrix_pretty_works() {
+    let mut cmd = Command::cargo_bin("rtf").unwrap();
+    let res = cmd
+        .arg("expand-matrix")
+        .arg("resources/sanity-check/test-plan.yaml")
+        .assert();
+
+    let expected_json = json!({
+        "variants": [{
+            "name": "matrix_variant_1",
+            "values": {"message": "hello, world!"}
+        }]
+    });
+
+    res.stdout(contains(
+        serde_json::to_string_pretty(&expected_json).unwrap(),
+    ));
+}
+
+#[test]
+fn expand_matrix_compact_works() {
+    let mut cmd = Command::cargo_bin("rtf").unwrap();
+    let res = cmd
+        .arg("expand-matrix")
+        .arg("resources/sanity-check/test-plan.yaml")
+        .arg("--compact")
+        .assert();
+
+    let expected_json = json!({
+        "variants": [{
+            "name": "matrix_variant_1",
+            "values": {"message": "hello, world!"}
+        }]
+    });
+
+    res.stdout(contains(serde_json::to_string(&expected_json).unwrap()));
 }
 
 #[test]
