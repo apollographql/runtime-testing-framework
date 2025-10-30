48dcf64c7e026cd447aa9cec18776e9f2f945866
SharkBaitDLS <SharkBaitDLS@users.noreply.github.com>
2025-10-30 20:25:32 +0000

    deploy: d9686723bd3980c2e5416ba063bbf9ccdd7ce597

--- rustdoc/rtf_docgen/sidebar-items.js
+++ rustdoc/rtf_docgen/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["find_fp_variants","get_docs","get_name","lookup_item","main","write_struct_docs","write_table_of_contents"]};
+window.SIDEBAR_ITEMS = {"fn":["document_inner_fields","document_inner_variants","document_item_content","find_fp_variants","generate_toc_link","get_docs","get_name","lookup_item","main","write_file_provider_docs","write_table_of_contents"],"static":["FILE_PROVIDER_IDS"]};
--- rustdoc/search.desc/rtf_docgen/rtf_docgen-desc-0-.js
+++ rustdoc/search.desc/rtf_docgen/rtf_docgen-desc-0-.js
@@ -1,1 +1,1 @@
-searchState.loadedDescShard("rtf_docgen", 0, "Returns all the IDs for the implementations within the …\nWrites the documentation for each variant and its fields. …\nWrites the table of contents for all the File Providers …")
+searchState.loadedDescShard("rtf_docgen", 0, "Writes out the documentation for a struct’s fields or an …\nReturns all the IDs for the implementations within the …\nGenerates the table of contents link for a File Provider\nWrites the top-level documentation for each file provider …\nWrites the table of contents for all the File Providers …")
