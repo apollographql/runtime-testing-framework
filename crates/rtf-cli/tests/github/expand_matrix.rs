3c0e96fc823d77d15f218ab645412184e1701eca
dandersonhall <67905850+dandersonhall@users.noreply.github.com>
2025-11-11 11:44:03 +0000

    Add expand-matrix tests for all valid github test plans

---
+++ crates/rtf-cli/tests/github/expand_matrix.rs
@@ -0,0 +1,44 @@
+use assert_cmd::Command;
+use predicates::str::contains;
+use serde_json::Value;
+
+#[ignore = "requires a valid GitHub API Token"]
+#[test]
+fn pretty_config_files() {
+    let test_plan_dir = "github-config-files";
+
+    let mut cmd = Command::cargo_bin("rtf").unwrap();
+    let res = cmd
+        .arg("expand-matrix")
+        .arg(format!("resources/valid/{test_plan_dir}/test-plan.yaml"))
+        .assert();
+
+    let matrix_json_str =
+        std::fs::read_to_string(format!("resources/valid/{test_plan_dir}/matrix.json"))
+            .expect("unable to load matrix.json");
+    let expected_json: Value = serde_json::from_str(&matrix_json_str).unwrap();
+
+    res.stdout(contains(
+        serde_json::to_string_pretty(&expected_json).unwrap(),
+    ));
+}
+
+#[test]
+fn pretty_file() {
+    let test_plan_dir = "github-file";
+
+    let mut cmd = Command::cargo_bin("rtf").unwrap();
+    let res = cmd
+        .arg("expand-matrix")
+        .arg(format!("resources/valid/{test_plan_dir}/test-plan.yaml"))
+        .assert();
+
+    let matrix_json_str =
+        std::fs::read_to_string(format!("resources/valid/{test_plan_dir}/matrix.json"))
+            .expect("unable to load matrix.json");
+    let expected_json: Value = serde_json::from_str(&matrix_json_str).unwrap();
+
+    res.stdout(contains(
+        serde_json::to_string_pretty(&expected_json).unwrap(),
+    ));
+}
