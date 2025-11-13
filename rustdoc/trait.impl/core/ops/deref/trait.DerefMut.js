59748f1a039ceeedc47dfef284d5bbec482a155a
sminez <sminez@users.noreply.github.com>
2025-11-13 22:16:46 +0000

    deploy: 37dc16e83b58378af8fd77e9aac29cc19cec30fe

--- rustdoc/trait.impl/core/ops/deref/trait.DerefMut.js
+++ rustdoc/trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rtf_config",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"rtf_config/providers/file/struct.NamedFileProvider.html\" title=\"struct rtf_config::providers::file::NamedFileProvider\">NamedFileProvider</a>"]]]]);
+    var implementors = Object.fromEntries([["rtf_config",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"rtf_config/providers/file/struct.NamedFileProvider.html\" title=\"struct rtf_config::providers::file::NamedFileProvider\">NamedFileProvider</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
