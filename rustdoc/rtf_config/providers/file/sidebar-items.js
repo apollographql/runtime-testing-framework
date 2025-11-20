1224f6a729d5d91ae4be823c11aa270b50484724
dandersonhall <dandersonhall@users.noreply.github.com>
2025-11-20 16:50:49 +0000

    deploy: ec2c4eb0b37e40e01c06767ba6899763f325d8e5

--- rustdoc/rtf_config/providers/file/sidebar-items.js
+++ rustdoc/rtf_config/providers/file/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["FileProvider","RawSource","Source"],"mod":["apollo","github","utility"],"struct":["InlineFile","NamedFileProvider","RelativeFile","RequiredFile"]};
+window.SIDEBAR_ITEMS = {"enum":["FileProvider","RawSource","Source"],"mod":["apollo","custom","github","utility"],"struct":["InlineFile","NamedFileProvider","RelativeFile","RequiredFile"]};
