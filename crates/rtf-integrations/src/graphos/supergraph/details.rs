01129e96e787d50b21cac62e9586ff011d119364
Robert Kirkum <robert.kirkum@apollographql.com>
2026-01-06 11:18:16 -0800

    Formatting

--- crates/rtf-integrations/src/graphos/supergraph/details.rs
+++ crates/rtf-integrations/src/graphos/supergraph/details.rs
@@ -416,11 +416,7 @@ fn replace_sourceless_connector_urls(schema: &mut Schema, base_url: &str) {
                     continue;
                 }
 
-                rewrite_connector_url(
-                    args_map,
-                    &http_verbs,
-                    &format!("{base_url}/{field_name}"),
-                );
+                rewrite_connector_url(args_map, &http_verbs, &format!("{base_url}/{field_name}"));
             }
         }
     }
