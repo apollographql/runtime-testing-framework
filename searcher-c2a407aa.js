07e649da8d029a9614dca5d1f4d9cb8a2ae82b3c
dandersonhall <dandersonhall@users.noreply.github.com>
2026-02-25 09:55:10 +0000

    deploy: 92e2e4042040353311aa2c74a283865c71df3c56

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6d6bbd0b.js',
+                path_to_root + 'searchindex-21192eb9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
