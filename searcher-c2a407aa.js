cbcb6fefc08e70015f7806b44368e19f62687ce0
sminez <sminez@users.noreply.github.com>
2026-02-27 15:03:45 +0000

    deploy: ff29ddf7651b937c3b3d6f9015156330aa7c16e2

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6d2d10e3.js',
+                path_to_root + 'searchindex-7adde950.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
