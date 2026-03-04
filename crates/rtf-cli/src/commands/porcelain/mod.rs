078246fc6292459de666266cdad08df1478b93cb
Innes Anderson-Morrison <innes.andersonmorrison@apollographql.com>
2026-03-04 10:00:40 +0000

    Add rtf docs command (#312)

--- crates/rtf-cli/src/commands/porcelain/mod.rs
+++ crates/rtf-cli/src/commands/porcelain/mod.rs
@@ -1,6 +1,8 @@
 //! High level user commands for running the most common workflows and functionality exposed
 //! through the rtf CLI.
 
+mod docs;
 mod run;
 
+pub use docs::open_docs;
 pub use run::check_and_run_test_plan;
