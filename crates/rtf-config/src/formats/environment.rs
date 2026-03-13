20f905fdd61ef46899d125399f64ee78837d8362
dandersonhall <67905850+dandersonhall@users.noreply.github.com>
2026-03-13 16:09:13 +0000

    Change default docker compose project name
    This removes the requirement for threading through the environment config name as a fall back which complicates making the EnvironmentExecution generic

--- crates/rtf-config/src/formats/environment.rs
+++ crates/rtf-config/src/formats/environment.rs
@@ -84,7 +84,7 @@ impl EnvironmentConfig {
     ) -> providers::Result<String> {
         match &self.execution {
             EnvironmentExecution::DockerCompose(inner) => {
-                inner.execute_setup(name, &self.name, out_dir, ctx).await
+                inner.execute_setup(name, out_dir, ctx).await
             }
             EnvironmentExecution::Script(inner) => inner.execute_setup(name, out_dir, ctx).await,
         }
@@ -97,9 +97,7 @@ impl EnvironmentConfig {
         ctx: &mut impl ResolutionContext,
     ) -> providers::Result<String> {
         match &self.execution {
-            EnvironmentExecution::DockerCompose(inner) => {
-                inner.execute_teardown(&self.name, ctx).await
-            }
+            EnvironmentExecution::DockerCompose(inner) => inner.execute_teardown(ctx).await,
             EnvironmentExecution::Script(inner) => inner.execute_teardown(name, out_dir, ctx).await,
         }
     }
@@ -325,15 +323,14 @@ pub struct DockerComposeEnvironment {
 }
 
 impl DockerComposeEnvironment {
-    fn project_name(&self, name: &str) -> String {
-        let raw = self.project_name.as_deref().unwrap_or(name);
+    fn project_name(&self) -> String {
+        let raw = self.project_name.as_deref().unwrap_or("rtf_environment");
         slugify_compose_project_name(raw)
     }
 
     pub async fn execute_setup(
         &self,
         name: &str,
-        env_name: &str,
         out_dir: &Path,
         ctx: &mut impl ResolutionContext,
     ) -> providers::Result<String> {
@@ -343,7 +340,7 @@ impl DockerComposeEnvironment {
         self.run_providers(&providers_dir.join(format!("{name}_providers")), ctx)
             .await?;
 
-        let project_name = self.project_name(env_name);
+        let project_name = self.project_name();
         let env_vars = self.all_env_vars(out_dir, &output_path, ctx)?;
         let (cmd, args) = self.setup_as_command_and_args(&project_name, ctx)?;
 
@@ -360,10 +357,9 @@ impl DockerComposeEnvironment {
 
     pub async fn execute_teardown(
         &self,
-        env_name: &str,
         ctx: &mut impl ResolutionContext,
     ) -> providers::Result<String> {
-        let project_name = self.project_name(env_name);
+        let project_name = self.project_name();
         let (cmd, args) = self.teardown_as_command_and_args(&project_name)?;
 
         ctx.run_command_blocking(cmd, args.iter().map(|s| s.as_str()), &HashMap::new())
