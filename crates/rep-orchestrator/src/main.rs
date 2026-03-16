4488ca40cdf551c8ba453a0e2741cf0a243d782a
Innes Anderson-Morrison <innes.andersonmorrison@apollographql.com>
2026-03-16 16:34:09 +0000

    Stub out initial axum server (#332)

--- crates/rep-orchestrator/src/main.rs
+++ crates/rep-orchestrator/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use rep_orchestrator::run_server;
+use std::{io::stdout, process};
+use tracing::{error, info, subscriber::set_global_default};
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+#[tokio::main]
+async fn main() {
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(stdout)
+        .json()
+        .flatten_event(true)
+        .with_span_list(true)
+        .with_current_span(false)
+        .finish();
+
+    // TODO: wire up a reload handle for runtime setting of the logging filter
+
+    set_global_default(subscriber).expect("unable to set a global tracing subscriber");
+
+    info!("starting server");
+    if let Err(error) = run_server().await {
+        error!(%error, "Fatal error");
+        process::exit(1);
+    }
 }
