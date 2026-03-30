7279c8498ff66b574a534bc113d503c6d35d4061
Innes Anderson-Morrison <innes.andersonmorrison@apollographql.com>
2026-03-30 16:23:59 +0100

    Add event handler for cleanup_namespace (#365)

---
+++ crates/rep-orchestrator/src/event_loop/cleanup_namespace.rs
@@ -0,0 +1,49 @@
+use crate::{
+    db::TestExecution,
+    event_loop::{Error, Result},
+    k8s,
+};
+use tracing::info;
+
+pub(super) async fn try_run<K>(test_execution: TestExecution, clients: K) -> Result<()>
+where
+    K: k8s::Client,
+{
+    let execution_id = test_execution.uuid();
+    info!(%execution_id, "deleting namespace");
+    clients
+        .delete_workload_namespace(&execution_id.to_string())
+        .await
+        .map_err(|error| Error::DeleteNamespace { error })?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::k8s::mock_client::{MockClient, Resp};
+
+    #[tokio::test]
+    async fn try_run_happy_path_returns_ok() {
+        let ex = TestExecution::create_stub(1, 1, "test");
+        let clients = MockClient::default_ok();
+
+        let res = try_run(ex, clients).await;
+
+        assert!(res.is_ok(), "{res:?}");
+    }
+
+    #[tokio::test]
+    async fn try_run_returns_expected_delete_error() {
+        let ex = TestExecution::create_stub(1, 1, "test");
+        let clients = MockClient {
+            delete_workload_namespace: Resp::new(Err(k8s::Error::Kube(kube::Error::TlsRequired))),
+            ..MockClient::default()
+        };
+
+        let res = try_run(ex, clients).await;
+
+        assert!(matches!(res, Err(Error::DeleteNamespace { .. })));
+    }
+}
