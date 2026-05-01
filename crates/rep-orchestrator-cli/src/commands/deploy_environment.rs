5c8c708329f5d7e7ba58665c6c9f89c05fef394a
Innes Anderson-Morrison <innes.andersonmorrison@apollographql.com>
2026-05-01 16:02:22 +0100

    Trying this with kustomize

--- crates/rep-orchestrator-cli/src/commands/deploy_environment.rs
+++ crates/rep-orchestrator-cli/src/commands/deploy_environment.rs
@@ -14,6 +14,35 @@ const DUPLICATE_ERROR: &str =
     "Encountered a duplicate environment entry in the resolved RTF environment";
 const MALFORMED_ERROR: &str = "Encountered a malformed line in the resolved RTF environment";
 const MISSING_EXPORT_ERROR: &str = "Expected leading 'export ' prefix to env file line";
+const KUSTOMIZE_PATCH: &str = r#"
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - kompose-output.yaml
+patches:
+  - target:
+      kind: Deployment
+      annotationSelector: "rtf.fileproviders=true"
+    patch: |-
+      apiVersion: apps/v1
+      kind: Deployment
+      metadata:
+        name: placeholder  # overridden by target selector
+      spec:
+        template:
+          spec:
+            initContainers:
+              - name: toolbox-init
+                image: us-central1-docker.pkg.dev/platform-cross-environment/apollo-private-docker/rtf-toolbox:edge
+                command:
+                  - rep-orchestrator-cli
+                  - resolve-environment
+                  - --outdir
+                  - /providers
+                volumeMounts:
+                  - mountPath: /providers
+                    name: providers
+"#;
 
 pub async fn deploy_environment(
     namespace: &str,
@@ -72,7 +101,7 @@ pub async fn deploy_environment(
                 "-n",
                 namespace,
                 "-f",
-                &k8s_dir_path.to_string_lossy(),
+                &k8s_dir_path.join("out.yaml").to_string_lossy(),
             ]),
         Status::Provisioning,
     )
@@ -117,9 +146,28 @@ async fn setup_env(
         Status::Provisioning,
         "Converting to kubernetes manifests..."
     )?;
-    let mut kompose = build_kompose_command(&compose_files_content, k8s_dir_path, &env_vars);
+    let mut kompose = build_kompose_command(
+        &compose_files_content,
+        &k8s_dir_path.join("kompose-output.yaml"),
+        &env_vars,
+    );
 
-    ctx.run_shell(&mut kompose, Status::Provisioning).await
+    ctx.run_shell(&mut kompose, Status::Provisioning).await?;
+
+    ctx.write_file(
+        &k8s_dir_path.join("kustomization.yaml"),
+        KUSTOMIZE_PATCH.as_bytes(),
+    )?;
+
+    ctx.run_shell(
+        Command::new("kubectl").args([
+            "kustomize",
+            "-o",
+            &k8s_dir_path.join("out.yaml").to_string_lossy(),
+        ]),
+        Status::Provisioning,
+    )
+    .await
 }
 
 /// Parse a `.env` file into a map of key-value pairs.
