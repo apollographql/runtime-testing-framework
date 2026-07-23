c8cbdc467301624ced4478f5000ccd4843533f58
sminez <sminez@users.noreply.github.com>
2026-07-23 14:04:44 +0000

    deploy: fa23b792c4aa4a70f96ae4fc750755fdd9763403

--- rustdoc/rtf_cli/commands/porcelain/sidebar-items.js
+++ rustdoc/rtf_cli/commands/porcelain/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["check_and_run_test_plan","ci_run","open_docs","pull_execution_output","pull_run_output","remote_run"]};
+window.SIDEBAR_ITEMS = {"fn":["check_and_run_test_plan","ci_run","execution_log","execution_status","open_docs","pull_execution_output","pull_run_output","remote_run","run_status"]};
