ca06ce70242563ed2206a2752dbbd29127b2bfe1
Innes Anderson-Morrison <innes.andersonmorrison@apollographql.com>
2026-09-11 10:09:52 +0100

    Remove now unused CLI subcommand

--- crates/rtf-orchestrator-cli/src/commands/mod.rs
+++ crates/rtf-orchestrator-cli/src/commands/mod.rs
@@ -1,13 +1,11 @@
 mod collect_output;
 mod create_namespace;
-mod create_service_account;
 mod deploy_environment;
 mod prepare_scenario;
 mod resolve_environment;
 
 pub use collect_output::collect_output;
 pub use create_namespace::create_namespace;
-pub use create_service_account::create_service_account;
 pub use deploy_environment::{ToolboxSettings, deploy_environment};
 pub use prepare_scenario::prepare_scenario;
 pub use resolve_environment::resolve_environment;
