f48be662aab70182ee59ebfce9ac2794849f63f2
sminez <sminez@users.noreply.github.com>
2026-09-16 13:45:28 +0000

    deploy: 3663064beed8d8028b3836e7ab1c0b53dd4d5696

--- rustdoc/rtf_docgen/sidebar-items.js
+++ rustdoc/rtf_docgen/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["document_inner_fields","document_inner_variants","document_item_content","find_fp_variants","generate_toc_link","get_docs","get_name","lookup_item","main","write_file_provider_docs","write_table_of_contents"],"static":["FILE_PROVIDER_IDS"]};
+window.SIDEBAR_ITEMS = {"fn":["item_docs","item_name","main"],"struct":["DocWriter"]};
