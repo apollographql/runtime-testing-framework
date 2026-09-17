ac50ca490fcfbecb630ae4408184e8298140b713
sminez <sminez@users.noreply.github.com>
2026-09-17 08:53:12 +0000

    deploy: 0030535fc41588a238ddf1e7706ca25478c789e5

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cbf6aaa5.js',
+                path_to_root + 'searchindex-57d88209.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
