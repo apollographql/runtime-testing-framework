d90beef9f164425d97496730c03fa253fdef09ce
Innes Anderson-Morrison <innes.andersonmorrison@apollographql.com>
2026-09-18 16:08:06 +0100

    Make file provider output modifiable when running under orchestrator (#570)

--- crates/rtf-orchestrator-cli/src/commands/resolve_environment.rs
+++ crates/rtf-orchestrator-cli/src/commands/resolve_environment.rs
@@ -1,5 +1,5 @@
 use crate::{context::CliContext, orchestrator::Client as OrchestratorClient};
-use std::{env::temp_dir, path::Path, process::Command};
+use std::{env::temp_dir, os::unix::fs::PermissionsExt, path::Path, process::Command};
 use tracing::info;
 
 /// This runs per-pod that needs to make use of file provider output within the environment
@@ -20,6 +20,22 @@ pub async fn resolve_environment(outdir: &str, ctx: &impl CliContext) -> crate::
         "-vvv",
     ]))
     .await?;
+
+    // Ensure that all files/directories we've written out are world writable and that directories
+    // can be traversed. We don't get this for free as we can't guarantee that this CLI is running
+    // under the same UID that the user provided container consuming the files is using.
+    for entry in ctx.list_entries_under(Path::new(outdir))? {
+        let mode = ctx.permissions(&entry.path)?.mode();
+        let additional = if entry.is_dir {
+            // write + execute (traverse) for owner/group/other
+            0o333
+        } else {
+            // write for owner/group/other
+            0o222
+        };
+
+        ctx.set_permissions_mode(&entry.path, mode | additional)?;
+    }
 
     info!("environment resolved successfully");
 
